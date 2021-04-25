7580fce07f8dd96a697de152be709882e93828fe
theodore-chronopoulos <75033645+theodore-chronopoulos@users.noreply.github.com>
2021-04-25 22:52:18 +0300

    home page-loign/register

---
+++ front-end/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import LoginRegister from './components/pages/LoginRegister'
+import Navbar from './components/Navbar';
+import Home from './components/pages/Home'
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          {/* <Route path='/' exact component=
+          {Home}/> */}
+          <Route path='/loginregister' exact component=
+            {LoginRegister}/>
+        </Switch>
+      </Router>
+    </div>
+    
+  );
+}
+
+export default App;
