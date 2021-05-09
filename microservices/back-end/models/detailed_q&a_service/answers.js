95c9b9aeafd3a50d6c9a785584dcac314db44259
pangiann <pangiann134ever@gmail.com>
2021-05-10 00:15:46 +0300

    New apis for answers, questions and users

--- microservices/back-end/models/detailed_q&a_service/answers.js
+++ microservices/back-end/models/detailed_q&a_service/answers.js
@@ -13,34 +13,35 @@ function CustomException(message, code) {
 }
 
 CustomException.prototype = Object.create(Error.prototype);
+client.connect();
 
 module.exports = {
-    insertAnswer: async function (username,  question_no, answer) {
-        await client.connect();
-        const users_collection = client.db('q&a').collection('Users');
+    insertAnswer: async function (user_id,  question_id, answer) {
         const answers_collection = client.db('q&a').collection('Answers');
+        const users_collection = client.db('q&a').collection('Users');
         const questions_collection = client.db('q&a').collection('Questions');
-        const query = {username: username};
+
         const projection = { projection: {_id:1} };
         const datetime = new Date();
         try {
-            const user_id = await users_collection.findOne(query, projection);
-            if (user_id === null) {
+            const query = {_id: user_id};
+            const user_id_returned = await users_collection.findOne(query, projection);
+            if (user_id_returned === null) {
                 throw new CustomException("User not Found", 404);
 
             }
             const update_res = await questions_collection.findOneAndUpdate(
-                {question_no: parseInt(question_no)},
+                {_id: question_id},
                 {$inc:{num_of_answers:1}},
                 {returnOriginal: false}
             );
+            console.log(update_res);
             if (update_res.value === null) {
                 throw new CustomException("Question Not Found", 404);
             }
             const answer_doc = {
-                user_id: user_id._id,
+                user_id: user_id,
                 question_id: update_res.value._id,
-                question_no: question_no,
                 date: datetime,
                 answer: answer,
                 upvotes: 0
@@ -53,6 +54,66 @@ module.exports = {
             throw error;
 
         }
+    },
+    showAnswersforQuestion: async function (question_id) {
+        try {
+            const query = {question_id: question_id};
+            const answers_collection = client.db('q&a').collection('Answers');
+            const result = await answers_collection.find(query).toArray();
+            return result;
+        }
+        catch (error) {
+            throw error;
+        }
+    },
+    showAnswersforUser: async function (user_id) {
+        try {
+            const query = {user_id: user_id};
+            const answers_collection = client.db('q&a').collection('Answers');
+            const result = await answers_collection.find(query).toArray();
+            return result;
+        }
+        catch (error) {
+            throw error;
+        }
+    },
+    deleteAnswer: async function (answer_id) {
+        const query = {_id: answer_id};
+        try {
+            const answers_collection = client.db('q&a').collection('Answers');
+            const result = await answers_collection.deleteOne(query);
+            console.log(result);
+            if (result.deletedCount === 0) {
+                throw new CustomException("Answer Not Found", 404);
+            }
+
+        }
+        catch (error) {
+            throw error;
+        }
+    },
+    updateAnswer: async function (answer_id, new_answer) {
+        const datetime = new Date();
+        try {
+            const answers_collection = client.db('q&a').collection('Answers');
+            const query = {_id: answer_id};
+            const newValues = {
+                $set: {
+                    date: datetime,
+                    answer: new_answer
+
+                }
+            };
+            const result = await  answers_collection.updateOne(query, newValues);
+            if (result.modifiedCount === 0) {
+                throw new CustomException("Answer Not Found", 404);
+            }
+            return result;
+        } catch (error) {
+            throw error;
+        }
+
+
     }
 
 };
--- microservices/back-end/models/detailed_q&a_service/questions.js
+++ microservices/back-end/models/detailed_q&a_service/questions.js
@@ -29,22 +29,22 @@ async function getNextSequenceValue(questions_collection, sequenceName){
 }
 
 module.exports = {
-    insertQuestion: async function (username,  title, question, keywords) {
+    insertQuestion: async function (user_id,  title, question, keywords) {
         const users_collection = client.db('q&a').collection('Users');
         const questions_collection = client.db('q&a').collection('Questions');
-        const query = {username: username};
+        const query = {_id:  user_id};
         const projection = { projection: {_id:1} };
         const datetime = new Date();
         try {
-            const user_id = await users_collection.findOne(query, projection);
-            if (user_id === null) {
+            const user_id_info = await users_collection.findOne(query, projection);
+            if (user_id_info === null) {
                 throw new CustomException("User Not Found", 404);
             }
             const seq = await getNextSequenceValue(questions_collection, "questionsInfo");
             console.log(seq);
             const question_doc = {
                 question_no: seq,
-                user_id: user_id._id,
+                user_id: user_id,
                 date: datetime,
                 title: title,
                 question: question,
@@ -89,6 +89,30 @@ module.exports = {
             throw error;
         }
     },
+    updateQuestion: async function (question_id, new_title, new_question, new_keywords) {
+        const datetime = new Date();
+        try {
+            const questions_collection = client.db('q&a').collection('Questions');
+            const query = {_id: question_id};
+            const newValues = {
+                $set: {
+                    date: datetime,
+                    question: new_question,
+                    title: new_title,
+                    keywords: new_keywords
+                }
+            };
+            const result = await  questions_collection.updateOne(query, newValues);
+            if (result.modifiedCount === 0) {
+                throw new CustomException("Question Not Found", 404);
+            }
+            return result;
+        } catch (error) {
+            throw error;
+        }
+
+
+    },
     findQuestionByKeywords: async function (keyword_array) {
         const query = {keywords: {$in: keyword_array } };
         console.log(keyword_array);
@@ -104,6 +128,28 @@ module.exports = {
         catch (error) {
             throw error;
         }
+    },
+    findQuestionByUser: async function (user_id) {
+        const query = {_id: user_id};
+        const users_collection = client.db('q&a').collection('Users');
+        const projection = { projection: {_id:1} };
+        try {
+            const user_id = await users_collection.findOne(query, projection);
+            if (user_id === null) {
+                throw new CustomException("User Not Found", 404);
+            }
+            const questions_collection = client.db('q&a').collection('Questions');
+            const result = await questions_collection.find(query).toArray();
+            console.log(result);
+            if (result.length === 0) {
+                throw new CustomException("No questions found with this user id", 404);
+            }
+            return result;
+        }
+        catch (error) {
+            throw error;
+        }
+
     }
 
 };
