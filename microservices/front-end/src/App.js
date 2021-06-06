214a1b05f0f81aa6f87a30586cdcb6fa37565e25
theodore-chronopoulos <el17021@mail.ntua.gr>
2021-06-06 20:56:31 +0300

    home page part 2

--- microservices/front-end/src/App.js
+++ microservices/front-end/src/App.js
@@ -3,6 +3,10 @@ import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import LoginRegister from './pages/LoginRegister';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Keywords from './components/Keywords/Keywords';
+import TimeSearch from './components/TimeSearch/TimeSearch';
+import AskQuestion from './components/AskQuestion/AskQuestion';
+import AnswerQuestion from './components/AnswerQuestion/AnswerQuestion';
 import Home from './pages/Home';
 
 
@@ -38,6 +42,15 @@ function App() {
                   {Home}/>
             <Route path='/loginregister' exact component=
                 {LoginRegister}/>
+            <Route path='/keywords' exact component=
+                  {Keywords}/>
+            <Route path='/timesearch' exact component=
+                {TimeSearch}/>
+            <Route path='/askquestion' exact component=
+                  {AskQuestion}/>
+            <Route path='/answerquestion' exact component=
+                {AnswerQuestion}/>
+
           </Switch>
           <Footer />
         </Router>
