e294646e24464d6129f6f639934236dc01985272
pangiann <pangiann134ever@gmail.com>
2021-06-08 19:03:38 +0300

    First service to deploy in heroky

---
+++ microservices/show_QA_service/models/questions.js
@@ -0,0 +1,129 @@
+const MongoClient = require('mongodb').MongoClient;
+const crypto = require('crypto');
+const createError = require('http-errors');
+const BSON = require('bson');
+const {ObjectID} = require("bson");
+
+// Replace the uri string with your MongoDB deployment's connection string.
+
+const url = "mongodb+srv://pangiann:panatha4ever@saas-60.7e7gc.mongodb.net/test?authSource=admin&replicaSet=atlas-o23qsd-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true";
+const client = new MongoClient(url, {useNewUrlParser: true,  useUnifiedTopology: true});
+client.connect();
+function CustomException(message, code) {
+    const error = new Error(message);
+    error.code = code;
+    return error;
+}
+
+
+
+// function to update number of questions
+// in 10000 years an overflow is highly probable to occur but who cares,
+// I'll not even exist until then, so good luck with the next ones
+CustomException.prototype = Object.create(Error.prototype);
+async function getNextSequenceValue(questions_collection, sequenceName){
+    const sequenceDocument = await questions_collection.findOneAndUpdate(
+        {_id: sequenceName},
+        {$inc:{seqValue:1}},
+        {returnOriginal: false}
+    );
+    return sequenceDocument.value.seqValue;
+}
+
+module.exports = {
+    // Here an event produced in the kafka bus by add_q&a service where a new question is added
+    // so, we have already q question_id, user_id, question_no, answer and date for this
+    // we create the document and insert it in the database
+    insertQuestion: async function (user_id,  username, question_id, title, question, question_no, date, keywords, num_of_answers) {
+        const questions_collection = client.db('questions_answers_only').collection('Questions');
+        try {
+            const question_doc = {
+                _id: question_id,
+                user_id: user_id,
+                username: username,
+                title: title,
+                question_no: question_no,
+                question: question,
+                date: date,
+                keywords: keywords,
+                num_of_answers: num_of_answers
+            }
+            const result = await  questions_collection.insertOne(question_doc);
+
+            // In the result of insertOne() function there is a field called insertedCount that is returned from mongodb
+            // With this value we could check if everything did go well (specifically insertedCount should be equal to 1)
+            // or else we should throw an error with 404 code
+            if (result.insertedCount !== 1) {
+                throw new CustomException("Question insertion failed", 404);
+            }
+            return result;
+        } catch (error) {
+            throw error;
+        }
+
+    },
+    showQuestions: async function () {
+        try {
+            const questions_collection = client.db('questions_answers_only').collection('Questions');
+            const result = await questions_collection.find().toArray();
+
+            return result;
+        }
+        catch (error) {
+            throw error;
+        }
+
+
+
+    },
+    deleteQuestion: async function (question_id) {
+        const query = {_id: question_id};
+        try {
+            const questions_collection = client.db('questions_answers_only').collection('Questions');
+            const result = await questions_collection.deleteOne(query);
+            //console.log(result);
+            if (result.deletedCount === 0) {
+                throw new CustomException("Question Not Found", 404);
+            }
+
+        }
+        catch (error) {
+            throw error;
+        }
+    },
+
+    // mongo has a nice way to compare
+    findQuestionByKeywords: async function (keyword_array) {
+        const query = {keywords: {$in: keyword_array } };
+        console.log(keyword_array);
+        try {
+            const questions_collection = client.db('questions_answers_only').collection('Questions');
+            const result = await questions_collection.find(query).toArray();
+            console.log(result);
+            if (result.length === 0) {
+                throw new CustomException("No questions found with these keywords", 404);
+            }
+            return result;
+        }
+        catch (error) {
+            throw error;
+        }
+    },
+    findQuestionByUser: async function (user_id) {
+        const query = {user_id: user_id};
+        try {
+
+            const questions_collection = client.db('questions_answers_only').collection('Questions');
+            const result = await questions_collection.find(query).toArray();
+            if (result.length === 0) {
+                throw new CustomException("No questions found with this user id", 404);
+            }
+            return result;
+        }
+        catch (error) {
+            throw error;
+        }
+
+    }
+
+};
---
+++ microservices/show_QA_service/routes/consumer.js
@@ -0,0 +1,78 @@
+//const users_model = require('models/users_profile_service/users')
+
+const questions = require('../models/questions');
+const answers = require('../models/answers');
+const {ObjectID} = require("bson");
+const { Kafka } = require('kafkajs');
+const MAX_RETRIES = 10;
+
+const kafka = new Kafka({
+    clientId: 'askMeAnything',
+    brokers: ['localhost:9093', "localhost:9094", "localhost:9095"]
+})
+
+const consumer = kafka.consumer({groupId: "ShowQ&AProfile"})
+
+const consume = async() => {
+    await consumer.connect();
+    const topic = "questions";
+    const topic2 = "answers";
+    let user_retries = 0;
+    let messages_topics_offsets = {};
+    let messages_topics_partition = {};
+    let last_topic;
+    let last_offset;
+    let last_partition;
+    await consumer.subscribe({topic: topic});
+    await consumer.subscribe({topic: topic2});
+    await consumer.run({
+        eachMessage: async ({topic, partition, message}) => {
+            const key = message.key.toString();
+            const value = message.value.toString();
+            //console.log(message.offset)
+            const value_json = JSON.parse(value);
+            //console.log(value_json)
+            messages_topics_offsets[topic] = message.offset;
+            last_topic = topic;
+            last_offset = parseInt(message.offset) + 1;
+            last_partition = partition;
+            messages_topics_partition[topic] = partition;
+            try {
+                if (message.key.toString() === "POST_QUESTION") {
+                    await questions.insertQuestion(ObjectID(value_json.user_id), value_json.username, ObjectID(value_json.question_id), value_json.title, value_json.question,
+                        value_json.question_no, value_json.date, value_json.keywords, value_json.num_of_answers);
+                }
+                if (message.key.toString() === "POST_ANSWER") {
+                    await answers.insertAnswer(ObjectID(value_json.answer_id), ObjectID(value_json.question_id), ObjectID(value_json.user_id),
+                        value_json.username, value_json.question_no, value_json.answer, value_json.date)
+                }
+                if (message.key.toString() === "NEW_UPVOTE") {
+                    await answers.updateNoUpvotes(ObjectID(value_json.answer_id));
+                }
+            }
+            catch(error) {
+                console.log(error);
+                setTimeout(function(){
+                    user_retries++;
+                    //console.log(user_retries);
+                    if (user_retries <= MAX_RETRIES) {
+                        consumer.seek({topic: topic, partition: partition, offset: message.offset})
+                    }
+                    else {
+                        console.log("hello")
+                        user_retries = 0;
+                        consumer.seek(({topic: last_topic, partition: last_partition, offset: last_offset.toString()}))
+
+                    }
+                }, 3000);
+
+            }
+
+        }
+
+    })
+}
+
+consume().catch((err) => {
+    console.error("error in consumer: ", err)
+})
