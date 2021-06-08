7badea876cbdc2ddf64f44380da40dc905d8cff4
theodore-chronopoulos <el17021@mail.ntua.gr>
2021-06-08 12:31:19 +0300

    front end part 4

--- microservices/front-end/src/components/AskQuestion/AskQuestion.jsx
+++ microservices/front-end/src/components/AskQuestion/AskQuestion.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../ChoicesBoxes/scss/style.scss';
+import './scss/style.scss';
 import answer from "../ChoicesBoxes/images/Answer.jpg";
 import ask from "../ChoicesBoxes/images/ask.jpeg";
 import statistics from "../ChoicesBoxes/images/statistics.jpg";
@@ -10,88 +10,80 @@ class AskQuestion extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          click: true
+          title: '',
+          description: '',
+          keyword1:'',
+          keyword2:'',
+          keyword3:''
         };
       }
 
-    closeMobileMenu = () => this.setState({click:false});
+    handleTitleChange = (event) => {this.setState({
+        title: event.target.value
+    })
+    }
+    handleDescriptionChange = (event) => {this.setState({
+        description: event.target.value
+    })
+    }
+    handleKeyword1Change = (event) => {this.setState({
+        keyword1: event.target.value
+    })
+    }
+    handleKeyword2Change = (event) => {this.setState({
+        keyword2: event.target.value
+    })
+    }
+    handleKeyword3Change = (event) => {this.setState({
+        keyword3: event.target.value
+    })
+    }
 
     render() {
         return (
-            <section className="choices">
-
-                <div className="choice__content container container--nav container--pall">
-
-                    <div className="choice__grid">
-
-                        <a href="#" className="choice__item">
-                            <div className="choice__image"
-                                 style={{backgroundImage: `url(${search_keyword})`}}>
-                            </div>
-
-                            <div className="choice__text">
-                                <div className="choice__title">
-                                    Questions per Keyword
-                                </div>
-                                <div className="choice__description">
-                                    View the most used keywords
-                                    and insert one to see all 
-                                    related questions.
-                                </div>
-                            </div>
-                        </a>
-                        <a href="#" className="choice__item">
-                            <div className="choice__image"
-                                 style={{backgroundImage: `url(${statistics})`}}>
-                            </div>
-
-                            <div className="choice__text">
-                                <div className="choice__title">
-                                    Questions per Time Period
-                                </div>
-                                <div className="choice__description">
-                                    Select a time period of your
-                                    preferance and see all the 
-                                    posted questions and answers.
-                                </div>
-                            </div>
-                        </a>
-                        <a href="#" className="choice__item">
-                            <div className="choice__image"
-                                 style={{backgroundImage: `url(${ask})`}}>
-                            </div>
-
-                            <div className="choice__text">
-                                <div className="choice__title">
-                                    Ask a new question
-                                </div>
-                                <div className="choice__description">
-                                    Struggling with finding answers
-                                    to a problem? Post a new question
-                                    and let our community take 
-                                    care the rest.
-                                </div>
-                            </div>
-                        </a>
-                        <a href="#" className="choice__item">
-                            <div className="choice__image"
-                                 style={{backgroundImage: `url(${answer})`}}>
-                            </div>
-
-                            <div className="choice__text">
-                                <div className="choice__title">
-                                    Answer a question
-                                </div>
-                                <div className="choice__description">
-                                    Are you Mr. BigBrains? Answer a
-                                    question and make someone's life easier.
-                                </div>
-                            </div>
-                        </a>
-                    </div>
+            <form>
+                <div>
+                    <label>Title</label>
+                    <input
+                    type="text"
+                    value={this.state.title}
+                    onChange={this.handleTitleChange}
+                    />
                 </div>
+                <div>
+                    <label>Description</label>
+                    <textarea
+                    type="text"
+                    value={this.state.description}
+                    onChange={this.handleDescriptionChange}
+                    />
+                </div>
+                <div className="keywords">
+                    <label>Keyword 1</label>
+                    <input
+                    type="text"
+                    value={this.state.keyword1}
+                    onChange={this.handleKeyword1Change}
+                    />
+                </div>
+                <div className="keywords">
+                    <label>Keyword 2</label>
+                    <input
+                    type="text"
+                    value={this.state.keyword2}
+                    onChange={this.handleKeyword2Change}
+                    />
+                </div>
+                <div className="keywords">
+                    <label>Keyword 3</label>
+                    <input
+                    type="text"
+                    value={this.state.keyword3}
+                    onChange={this.handleKeyword3Change}
+                    />
+                </div>
+            </form>
 
-            </section>
         );
     }
 }
--- microservices/front-end/src/components/TimeSearch/TimeSearch.jsx
+++ microservices/front-end/src/components/TimeSearch/TimeSearch.jsx
@@ -1,192 +1,157 @@
 import React, { useState } from 'react'
+
 import './scss/App.css';
-import DatePicker from 'react-datepicker';
-import TimeKeeper from 'react-timekeeper';
+import './scss/BalanceTable.css';
+
 import moment from 'moment';
 import pointimg from "../ChoicesBoxes/images/statistics.jpg";
 import "react-datepicker/dist/react-datepicker.css"
 import { Link } from 'react-router-dom';
 import Swal from 'sweetalert2'
+import { DateRangePicker } from 'react-date-range';
+
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
 // import getCookie from '../../functions/getCookie.js'
 // import jwt_decode from "jwt-decode";
 
 
+class TimeSearch extends React.Component {    
 
-
-
-// function reserveSlot(point_id,effective_timestamp){
-//   effective_timestamp = "asdasd";
-//   var reservation_html = document.getElementById("reservation-details").outerHTML;
-//   // var reservation_html = <div>asd</div>;
-//   Swal.fire({
-//     title: '<strong>Your reservation details</strong>',
-//     html: reservation_html,
-//     showCloseButton: true,
-//     showCancelButton: true,
-//     focusConfirm: false,
-//     confirmButtonText: "Yes , reserve slot",
-//     cancelButtonText: "No , do not reserve"
-//   }).then((result) => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      transactions:[],
+      sessions:[],
+      start_date: new Date(),
+      end_date:  new Date(),
+      current_balance: 0,
+      current_wallet: 0,
+      balance_fetched: false
+    };
     
-//     if (result.isConfirmed){
-//       var decoded = jwt_decode( getCookie('charge_evolution_token'));      
-//       var myHeaders = new Headers();
-//       myHeaders.append("charge_evolution_token", getCookie("charge_evolution_token"));
-//       myHeaders.append("Content-Type", "application/json");
+    this.handleSelect = this.handleSelect.bind(this);
+    // this.fetchDataApi = this.fetchDataApi.bind(this);
+    // this.fetchDataApi2 = this.fetchDataApi2.bind(this);
+    // this.parentCallback = this.parentCallback.bind(this);
+  }
+
+
+  // fetchDataApi(){
+  //     var decoded = jwt_decode( getCookie('charge_evolution_token'));
+  //     var myHeaders = new Headers();
+  //     myHeaders.append("Content-Type", "application/json");
+  //     myHeaders.append("charge_evolution_token",  getCookie('charge_evolution_token'));
+  //     var requestOptions = { method: 'GET', headers: myHeaders, redirect: 'follow' };
+  //     fetch("http://localhost:5000/api/users/transactions/"+ decoded.id + "/2000-01-01/3000-01-01" , requestOptions)
+  //     .then(response => response.json())
+  //     .then(result => {
+  //         // console.log(result);
+  //         this.setState({transactions:result}, () => console.log(this.state.transactions)  );
+  //     })
+  //     .catch(
+  //       err => console.log("ERROR")
+  //     )
+  //     fetch("http://localhost:5000/api/users/sessions/"+ decoded.id + "/2000-01-01/3000-01-01" , requestOptions)
+  //     .then(response => response.json())
+  //     .then(result => {
+  //         this.setState({sessions:result});
+  //     });
       
-//       var raw = JSON.stringify({"point_id":point_id,"effective_timestamp":effective_timestamp});
-//       var requestOptions = { method: 'POST', headers: myHeaders, body: raw, redirect: 'follow'};
+  //     fetch("http://localhost:5000/api/users/wallet/"+ decoded.id  , requestOptions)
+  //     .then(response => response.json())
+  //     .then(result => {
+  //         this.setState({current_wallet:result.wallet});
+  //         console.log(result)
+  //     })
+  //     .catch(e => {
+  //       console.log(e)
+  //     });
+  // }
+  
+  // fetchDataApi2(from,to){
+  //   var decoded = jwt_decode( getCookie('charge_evolution_token'));
+  //   var myHeaders = new Headers();
+  //   myHeaders.append("Content-Type", "application/json");
+  //   myHeaders.append("charge_evolution_token",  getCookie('charge_evolution_token'));
+  //   var requestOptions = { method: 'GET', headers: myHeaders, redirect: 'follow' };
+  //   fetch("http://localhost:5000/api/users/transactions/"+ decoded.id + "/" + from + "/" + to, requestOptions)
+  //   .then(response => response.json())
+  //   .then(result => {
+  //       // console.log(result);
+  //       this.setState({transactions:result}, () => console.log(this.state.transactions)  );
+  //   })
+  //   .catch(
+  //     err => console.log("ERROR")
+  //   )
+  //   fetch("http://localhost:5000/api/users/sessions/"+ decoded.id + "/" + from + "/" + to, requestOptions)
+  //   .then(response => response.json())
+  //   .then(result => {
+  //       this.setState({sessions:result});
+  //   });        
+  // }
+
+
+  // componentDidMount(){
+  //   this.setState({
+  //     balance_fetched:false
+  //   });
+  //   this.fetchDataApi();
+  //   }
+
+  // parentCallback = (data) => {
+  //   if (this.state.balance_fetched) return;
+  //   console.log(data)
+  //   this.setState({
+  //     balance_fetched:true,
+  //     current_balance:data
       
-//       fetch("http://localhost:5000/api/users/reservations/" + decoded.id, requestOptions)
-//         .then(response => response.json())
-//         .then(result => {
-//           if (result.status == 200) {
-//             Swal.fire({
-//                 title: 'Success',
-//                 text: result.msg,
-//                 icon: 'success',
-//                 customClass: "swal_ok_button",
-//                 confirmButtonColor: "#242424"
-//               });
-//         }
-//         else {
-//             Swal.fire({
-//                 title: 'Error!',
-//                 text: result.msg,
-//                 icon: 'error',
-//                 customClass: "swal_ok_button",
-//                 confirmButtonColor: "#242424"
-//               })
-//         }
-//         })
-//         .catch(error => console.log('error', error));
-//     }
+  //   });
+  // }
+
+  handleSelect(ranges){
+    // this.fetchDataApi2(ranges.selection.startDate.toISOString().slice(0,10),ranges.selection.endDate.toISOString().slice(0,10));
+    this.setState({
+      start_date: ranges.selection.startDate,
+      end_date: ranges.selection.endDate
+    });
+  }
+
+
+  
+
+  render () {
     
-          
-//     })
+      const selectionRange = {
+        startDate: this.state.start_date,
+        endDate: this.state.end_date,
+        key: 'selection',
+      }
 
-// }
-
-
-
-
-
-function TimeSearch (props) {
-//   console.log(props.location.state);
-//   const station = props.location.state.selected_station;
-//   const point = props.location.state.selected_point;
-//   if (point.fast_charging === 'True') var fast_charging = "Capable of fast charging" ;
-//   if (point.fast_charging === 'False') var fast_charging = "Not capable of fast charging"; 
-//   if (point.fast_charging === 'Uknown') var fast_charging = "Not capable of fast charging";
-//   const fast_charging_const = fast_charging;
-//   const connector = point.title + " " + point.level + " " + point.current 
-  const [selectedDate, setSelectedDate] = useState(new Date())
-  const [time, setTime] = useState('12:35')
-  return (
-    <div>
-      <div className="containercarimg">
-        <img src={pointimg} className="resize_fit_center" /> 
-        <br></br>
-        <div className="rowrendezvous">
-          <div className="columnrendezvous">
-              <div className="pane-title-container" >
-                <h2>Details of your reservation</h2>
-              </div>
-              <br></br>
-              <div id='reservation-details' className="randezvous-pane">
-                <p>Address: 
-                    {/* {station.address} */}
-                    </p>              
-                <p>Town: 
-                    {/* {station.town} */}
-                </p>
-                <p>Postcode: 
-                    {/* {station.postcode}  */}
-                </p>
-                <p>Contact Phone: 
-                    {/* {station.phone} */}
-                    </p>            
-                <p>Point specs: 
-                    {/* {connector} */}
-                </p>
-                <p>Fast charging: 
-                    {/* {fast_charging_const} */}
-                    </p>
-                <hr></hr>
-                <p><b>Date: 
-                    {moment(selectedDate).format('YYYY-MM-DD')}
-                </b> </p>
-                <p>Time: {time} </p>
-              </div>
-            </div>
-        
-          <div className='columnrendezvous'>
-            <div className="pane-title-container">
-              <h2>Pick date</h2>
-            </div>
-            
-            <div className="pane-title-container">
-              <DatePicker
-                className='react-datepicker'
-                selected={selectedDate}
-                onChange={(timetoshow) => setSelectedDate(timetoshow) }
-                dateFormat="dd/MM/yyyy"
-                placeholderText={'DD/MM/YYYY'}
-                minDate={new Date()}
-                showYearDropdown
-                scrollableYearDropdown
-                popperPlacement='bottom-end'
-                    popperModifiers={{
-                        flip: {
-                            enabled: false
-                        }}}
+        return (
+          <>
+          <div className="balance-title-container">
+            <div className="balance-title"><h1><i class="fas fa-balance-scale"></i> &nbsp;&nbsp;Balance overview</h1></div>
+          </div>
+          <div className='date-range-container'>
+              
+              <DateRangePicker
+                ranges={[selectionRange]}
+                onChange={this.handleSelect}
               />
-            </div>
-            
           </div>
-          <div className="columnrendezvous">
-            <div className="pane-title-container">
-              <h2>Pick time</h2>
-            </div>
-            <br></br>
-            <div  className="pane-title-container">
-              <div className="clockstyle">
-              <TimeKeeper
-                time={time}
-                onChange={(newTime) => setTime(newTime.formatted24)}
-                switchToMinuteOnHourSelect
-                hour24Mode
-                coarseMinutes={5}
-                forceCoarseMinutes
-              />
-              </div>
-            </div>
-          </div>
-          <div className="columnrendezvous">
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          <br></br>
-          
-            <button type="button" className='btn--large4'
-            //  onClick={ () =>  reserveSlot(point.id , moment(selectedDate).format('YYYY-MM-DD') + " :00")}
-             >
-            <i class="fas fa-calendar-check"></i>&nbsp;Book charging slot now!
-            </button>
-          
-          </div>
-          
-        </div>
-        <br></br>
-          <br></br>
-          <br></br>
-      </div>
-      
-    </div>
-    
-  )
+          {/* <BalanceTable 
+              transactions={this.state.transactions}
+              sessions={this.state.sessions}
+              parentCallback = {this.parentCallback}
+              >
+
+          </BalanceTable> */}
+          </>
+
+        );
+  }
+
 }
-
 export default TimeSearch
