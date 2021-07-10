01b98b692637ca162c832e9649c662d84dbccb1f
Panagiotis Giannoulis <pangiann@arrikto.com>
2021-07-10 18:54:27 +0300

    Change urls for deploy

--- soa/front-end/src/base_url.js
+++ soa/front-end/src/base_url.js
@@ -1,7 +1,7 @@
-export let login_register_url = "http://localhost:3000/loginRegister"
-export let qa_url = "http://localhost:3002/questionsAnswers"
-export let analytics_url = "http://localhost:3001/analytics"
+export let login_register_url = "https://login-register-service.herokuapp.com/loginRegister"
+export let qa_url = "https://soa-qa-management.herokuapp.com/questionsAnswers"
+export let analytics_url = "https://soa-analytics.herokuapp.com/analytics"
 
-export let bus_login_url = "http://localhost:3005/bus/loginRegister"
-export let bus_qa_url = "http://localhost:3005/bus/qa_management"
-export let bus_analytics_url = "http://localhost:3005/bus/analytics"
+export let bus_login_url = "https://soa-event-bus.herokuapp.com/bus/loginRegister"
+export let bus_qa_url = "https://soa-event-bus.herokuapp.com/bus/qa_management"
+export let bus_analytics_url = "https://soa-event-bus.herokuapp.com/bus/analytics"
